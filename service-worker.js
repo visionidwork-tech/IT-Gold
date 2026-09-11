f6c25fdc56d45df477ce5dbbe8e06a36dea01671
IT Gold Automation <visionidwork@gmail.com>
2026-09-11 17:40:15 +0000

    Add existing customer referral registration entry

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='itgold-v8';
-const ASSETS=['/','/share.html','/referrals.html','/renewal.html','/manifest.webmanifest','/itgold-icon.svg'];
+const CACHE='itgold-v10';
+const ASSETS=['/','/share.html','/existing-customer.html','/referrals.html','/renewal.html','/manifest.webmanifest','/itgold-icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
