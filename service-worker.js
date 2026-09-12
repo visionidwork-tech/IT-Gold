e295599028cb2f90fd55d10e11309742f43e100a
IT Gold Automation <visionidwork@gmail.com>
2026-09-12 02:15:05 +0000

    Only show install control when browser offers installation

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='itgold-v12';
+const CACHE='itgold-v13';
 const ASSETS=['/','/share.html','/existing-customer.html','/referrals.html','/renewal.html','/manifest.webmanifest','/itgold-icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
